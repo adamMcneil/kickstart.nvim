84d07134209de255c1393a21454e273d730dcdb3
adamMcneil <adamwmcneil@gmail.com>
2024-08-30 14:05:33 -0500

    changed theme

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,9 +1,9 @@
 return {
   'akinsho/bufferline.nvim',
-  version = "*",
+  version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    local bufferline = require("bufferline")
+    local bufferline = require 'bufferline'
     bufferline.setup {
       options = {
         style_preset = bufferline.style_preset.minimal,
@@ -12,19 +12,19 @@ return {
         },
         offsets = {
           {
-            filetype = "NvimTree",
-          }
+            filetype = 'NvimTree',
+          },
         },
         show_buffer_close_icons = false,
         show_close_icon = false,
-        separator_style = "thin",
-        diagnostics = "nvim_lsp",
+        separator_style = 'thin',
+        diagnostics = 'nvim_lsp',
         diagnostics_update_in_insert = false,
         -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
-          return "(" .. count .. ")"
+          return '(' .. count .. ')'
         end,
-      }
+      },
     }
-  end
+  end,
 }
